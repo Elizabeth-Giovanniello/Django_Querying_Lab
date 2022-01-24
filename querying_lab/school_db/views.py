8e3f525dd40110f12c969e1637a53a717a5c9114
dstoffels <dan.stoffels@gmail.com>
2022-01-24 14:14:51 -0500

    ordered a+ students by first name

--- querying_lab/school_db/views.py
+++ querying_lab/school_db/views.py
@@ -40,9 +40,7 @@ def problem_two(request):
 def problem_three(request):
     # Find all students who have a A+ in any class and are NOT getting a C+ in any class. 
     # Order the data by student's first name alphabetically.
-    students_with_a_plus = StudentCourse.objects.filter(grade='A+')
-
-    data_visualization = [item for item in students_with_a_plus]
+    students_with_a_plus = StudentCourse.objects.filter(grade='A+').order_by('student__first_name')
 
     context = {
         'student_courses': students_with_a_plus
